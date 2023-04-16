eaec660984047bdc5e61bd94cd5e9e15f4fd0648
emppi2007 <makineneemil97@gmail.com>
2023-04-16 14:04:03 +0300

    animaatiot valmiit pelaajalle

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,32 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-
-{
-
-
+public class PlayerMovement : MonoBehaviour {
+ public Animator animator;
+    private Vector2 movement;
 
 
 
 
-    public float speed = 5f; // hahmon liikkumis nopeus
+
+    public float movementSpeed = 5f; // hahmon liikkumis nopeus
     // Start is called before the first frame update
     void Start()
     {
-        
+       animator = GetComponent<Animator>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxisRaw("Vertical");
-        
+        if (movement != Vector2.zero) // (0, 0)
+        {
+            var xMovement = movement.x * movementSpeed * Time.deltaTime;
+            this.transform.Translate(new Vector3(xMovement, 0), Space.World);
 
-        Vector2 movement = new Vector2(horizontalInput, verticalInput).normalized*speed*Time.deltaTime;
+        }
+        movement =  new Vector2(Input.GetAxis("Horizontal"), 0).normalized;
+        animator.SetFloat("Speed", Mathf.Abs(movement.magnitude* movementSpeed));
 
-        transform.position += (Vector3)movement;
+        bool flipped = movement.x < 0;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f, flipped ? 180f : 0f, 0f));
     }
-}
+    
+
+        
+}   
